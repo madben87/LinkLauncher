6c16954c057a30d94d9648d165ee43ce66766a93
boychenko <boychenko.yuriy87@gmail.com>
2018-09-16 21:10:34 +0300

    update list bug (sort)

---
+++ app/src/main/java/ben/com/linklauncher/util/MessageEvent.java
@@ -0,0 +1,13 @@
+package ben.com.linklauncher.util;
+
+public class MessageEvent {
+
+    public static final int UPDATED_DB = 1;
+    public static final int UPDATED_VIEW= 2;
+
+    public final int msg;
+
+    public MessageEvent(int message) {
+        this.msg = message;
+    }
+}
